37e3dd5131b9760550f15562fba06bdfb0ed4b3c
EMERSON <elrn1979@gmail.com>
2022-07-19 09:55:28 -0300

    alterando

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -4,9 +4,9 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 22;
-		int b = 20;
-		System.out.println("Aqui é Galo!!!" + (a+b));
+		int a = 2;
+		int b = 2;
+		System.out.println("Aqui é Galo!" + (a+b));
 	}
 
 }
