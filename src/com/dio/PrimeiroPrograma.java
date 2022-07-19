75b5408b044c6b7bab99fd6932334e3cf1cfdd2a
EMERSON <elrn1979@gmail.com>
2022-07-19 09:37:41 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 22;
+		int b = 20;
+		System.out.println("Aqui é Galo!!!" + (a+b));
+	}
+
+}
